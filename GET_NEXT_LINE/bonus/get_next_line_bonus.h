a4cd602458c1cc45e73ee0f531a1d5d5a198f672
hsachie <sachie.duck.1113@icloud.com>
2026-07-01 19:37:20 +0900

    bonus

---
+++ GET_NEXT_LINE/bonus/get_next_line_bonus.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsachie <hsachie@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 12:44:33 by hsachie           #+#    #+#             */
+/*   Updated: 2026/07/01 19:36:00 by hsachie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# ifndef EOF
+#  define EOF -1
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+typedef struct s_buf
+{
+    char    buf[BUFFER_SIZE];
+    char    *bufp;
+    int     n;
+}   t_buf;
+
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+int					ft_putc(t_string *str, unsigned char c);
+char				*get_next_line(int fd);
+
+#endif
